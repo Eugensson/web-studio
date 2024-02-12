6ad9cbaa6c67f2b83a0e6047755ad44d3b2e26e1
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2024-02-12 19:19:44 +0200

    Updated config.js

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,3 @@
-// import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-// import { defineConfig } from 'vite';
-// import { resolve } from 'path';
-
-// const config = defineConfig(() => {
-//   return {
-//     plugins: [
-//       ViteImageOptimizer({
-//         test: /\.(jpe?g|png|gif|tiff|webp|avif)$/i,
-//       }),
-//     ],
-//     base: '/',
-//     build: {
-//       rollupOptions: {
-//         input: {
-//           main: resolve(__dirname, 'index.html'),
-//           portfolio: resolve(__dirname, 'portfolio/index.html')
-//         }
-//       }
-//     }
-//   };
-// });
-
-// export default config;
-
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
